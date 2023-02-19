138f6547a003b06e1f4d02c9dfe7d28bd0ef8b4d
Axel Brisse <axel.brisse@gmail.com>
2023-02-19 08:35:06 +0100

    pipes seem to work, fd_in and fd_out untested

--- srcs/main.c
+++ srcs/main.c
@@ -6,13 +6,13 @@
 /*   By: axbrisse <axbrisse@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/09 16:41:13 by axbrisse          #+#    #+#             */
-/*   Updated: 2023/02/19 07:56:05 by axbrisse         ###   ########.fr       */
+/*   Updated: 2023/02/19 08:23:22 by axbrisse         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 
-int	perror3(char *s1, char *s2, char *s3, int return_value)
+int	perror_custom(char *s1, char *s2, char *s3, int return_value)
 {
 	char	*message;
 	char	*full_error;
@@ -34,6 +34,19 @@ int	perror3(char *s1, char *s2, char *s3, int return_value)
 	return (return_value);
 }
 
+// TODO not ft_perror
+int	ft_perror(char *info, int return_value)
+{
+	char	*full;
+
+	full = ft_strjoin("pipex: ", info);
+	if (full == NULL)
+		return (perror("malloc"), EXIT_FAILURE);
+	perror(full);
+	free(full);
+	return (return_value);
+}
+
 int	main(int argc, char **argv, char **env)
 {
 	char	**path;
@@ -44,13 +57,21 @@ int	main(int argc, char **argv, char **env)
 	pid_t	pid;
 	char	**cmd_argv;
 	char	*full_path;
+	char	*file_in;
+	char	*file_out;
 	int		fd_in;
 	int		fd_out;
 
 	if (!check_args(argc, argv, &is_heredoc, &num_children))
 		return (EXIT_FAILURE);
-	fd_in = open(argv[1 + is_heredoc], O_RDONLY);
-	fd_out = open(argv[argc - 1], O_CREAT | O_WRONLY | O_TRUNC, 0644);
+	file_in = argv[1 + is_heredoc];
+	fd_in = open(file_in, O_RDONLY);
+	if (fd_in == -1)
+		return (ft_perror(file_in, EXIT_FAILURE));
+	file_out = argv[argc - 1];
+	fd_out = open(file_out, O_CREAT | O_WRONLY | O_TRUNC, 0644);
+	if (fd_out == -1)
+		return (ft_perror(file_out, EXIT_FAILURE));
 	pipes = init_pipes(num_children - 1);
 	if (pipes == NULL)
 		return (EXIT_FAILURE);
@@ -63,13 +84,15 @@ int	main(int argc, char **argv, char **env)
 			return (perror("fork"), EXIT_FAILURE);
 		else if (pid == 0)
 		{
-			if (i == 0)
+			if (i < num_children - 1)
 			{
-				ft_close(&pipes[0][0]);
+				ft_close(&pipes[i][0]);
+				dup2(pipes[i][1], STDOUT_FILENO);
+				ft_close(&pipes[i][1]);
 				dup2(fd_in, STDIN_FILENO);
-				dup2(pipes[0][1], STDOUT_FILENO);
+				ft_close(&fd_in);
 			}
-			else if (i == num_children - 1)
+			else
 			{
 				ft_close(&pipes[i - 1][1]);
 				dup2(pipes[i - 1][0], STDIN_FILENO);
@@ -80,16 +103,24 @@ int	main(int argc, char **argv, char **env)
 				return (perror("malloc"), EXIT_FAILURE);
 			full_path = find_absolute_path(path, cmd_argv[0]);
 			if (full_path == NULL)
-				return (perror3(cmd_argv[0], ": ", "command not found",
+				return (perror_custom(cmd_argv[0], ": ", "command not found",
 						RET_NOT_FOUND));
 			execve(full_path, cmd_argv, env);
 			return (perror("execve"), EXIT_FAILURE);
 		}
+		if (i != num_children - 1)
+		{
+			ft_close(&pipes[i][1]);
+			fd_in = pipes[i][0];
+		}
+		else
+		{
+			clean_pipes(pipes, num_children - 1);
+			ft_free_double_pointer((void ***)&path, SIZE_MAX);
+			while (waitpid(-1, NULL, 0) != -1)
+				;
+		}
 		++i;
 	}
-	clean_pipes(pipes, num_children - 1);
-	ft_free_double_pointer((void ***)&path, SIZE_MAX);
-	while (waitpid(-1, NULL, 0) != -1)
-		;
 	return (EXIT_SUCCESS);
 }
